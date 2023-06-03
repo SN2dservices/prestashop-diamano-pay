061ad1b3fd76d117b9bcd3dd9428f707f00ec22c
dieyne drame <dieynedrame@gmail.com>
2023-06-03 20:47:54 +0200

    WIP

---
+++ diamanopay/diamanopay.php
@@ -0,0 +1,118 @@
+<?php
+/*
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2015 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+require_once(dirname(__FILE__) . '/controllers/hook/content.php');
+class DiamanoPay extends PaymentModule
+{
+    protected $_html = '';
+    protected $_postErrors = array();
+
+    public $details;
+    public $owner;
+    public $address;
+    public $extra_mail_vars;
+
+    public function __construct()
+    {
+        $this->name = 'diamanopay';
+        $this->tab = 'payments_gateways';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->author = '2dServices';
+        $this->controllers = array('validation');
+        $this->is_eu_compatible = 1;
+
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
+
+        $this->bootstrap = true;
+        parent::__construct();
+
+        $this->displayName = $this->l('Diamano Pay');
+        $this->description = $this->l('Plateforme de paiement au Sénégal');
+
+        if (!count(Currency::checkPaymentCurrencies($this->id))) {
+            $this->warning = $this->l("Aucune devise n'est configurée pour ce module.");
+        }
+    }
+
+    public function install()
+    {
+        if (!parent::install() || !$this->registerHook('paymentOptions') || !$this->registerHook('paymentReturn')) {
+            return false;
+        }
+        return true;
+    }
+
+    public function hookPaymentOptions($params)
+    {
+        if (!$this->active) {
+            return;
+        }
+
+        if (!$this->checkCurrency($params['cart'])) {
+            return;
+        }
+
+        $payment_options = [
+            $this->getExternalPaymentOption(),
+        ];
+
+        return $payment_options;
+    }
+
+    public function checkCurrency($cart)
+    {
+        $currency_order = new Currency($cart->id_currency);
+        $currencies_module = $this->getCurrency($cart->id_currency);
+
+        if (is_array($currencies_module)) {
+            foreach ($currencies_module as $currency_module) {
+                if ($currency_order->id == $currency_module['id_currency']) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public function getExternalPaymentOption()
+    {
+        $externalOption = new \PrestaShop\PrestaShop\Core\Payment\PaymentOption();
+        $externalOption->setCallToActionText($this->l('Diamano pay'))
+            ->setAction($this->context->link->getModuleLink($this->name, 'validation', array(), true))
+            ->setAdditionalInformation($this->context->smarty->fetch('module:diamanopay/views/templates/front/payment_infos.tpl'))
+            ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_ . $this->name . '/payment.jpg'));
+
+        return $externalOption;
+    }
+    public function getContent()
+    {
+        $contentController = new ContentController($this);
+        return $contentController->run();
+    }
+}
---
+++ diamanopay/views/templates/hook/content.tpl
@@ -0,0 +1,3 @@
+{if isset($contentSubmitted)}
+    <div class="alert alert-success">{l s='Configuration sauvegardée avec succès!' mod='diamanopay'}</div>
+{/if}
